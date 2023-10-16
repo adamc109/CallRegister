ae14fc3683e2137883e8ddb88f63a1befcb1eb3e
Adam <adam1092002@icloud.com>
2023-10-16 21:44:38 +0100

    Add project files.

---
+++ CallRegisterWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CallRegisterWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
