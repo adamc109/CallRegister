cdc3db5d5307b2ec9e6c8d64df487edb9e6d5a15
Adam <adam1092002@icloud.com>
2023-10-17 22:18:33 +0100

    Email Model

---
+++ CallRegisterWeb/Models/Email.cs
@@ -0,0 +1,15 @@
+﻿namespace CallRegisterWeb.Models
+{
+    public class Email
+    {
+
+        public int Id { get; set; }
+        public string Agent { get; set; }
+        public DateOnly AllocatedDate { get; set; }
+        public DateOnly DateDue { get; set; }
+        public DateOnly DateCompleted { get; set; }
+        public Boolean Internal { get; set; }
+        public String ProductType { get; set; }
+
+    }
+}
