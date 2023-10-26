511bc5b1ca1a5b9c0add8bd45e26d06096617831
Adam <adam1092002@icloud.com>
2023-10-26 21:47:58 +0100

    Bind Agent Model to view

--- CallRegisterWeb/Controllers/EmailController.cs
+++ CallRegisterWeb/Controllers/EmailController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CallRegisterWeb.Data;
+using CallRegisterWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CallRegisterWeb.Controllers
 {
     public class EmailController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public EmailController(ApplicationDbContext db)
+        {
+            _db= db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Agent> objAgentList = _db.Agents.ToList();
+            return View(objAgentList);
         }
     }
 }
