c37c62b609ed88afb5b6a8f2689b6a0f9953436e
Adam <adam1092002@icloud.com>
2023-11-07 22:03:36 +0000

    Model Bind Add  Agent

--- CallRegisterWeb/Models/Agent.cs
+++ CallRegisterWeb/Models/Agent.cs
@@ -1,9 +1,13 @@
-﻿namespace CallRegisterWeb.Models
+﻿using System.ComponentModel;
+
+namespace CallRegisterWeb.Models
 {
     public class Agent
     {
         public int Id { get; set; }
+        [DisplayName("Agent Name")]
         public string Name { get; set; }
+        [DisplayName("Agent Team")]
         public string Team { get; set; }
     }
 }
