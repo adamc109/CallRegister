117bd38a715725e35f42fc599278006877a258e2
Adam <adam1092002@icloud.com>
2023-11-30 22:07:41 +0000

    Repository

--- CallRegister.DataAccess/Repository/IRepository/IRepository.cs
+++ CallRegister.DataAccess/Repository/IRepository/IRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CallRegister.DataAccess.Repository.IRepository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         //T - Agent
         IEnumerable<T> GetAll();
