65408e0352c1136b39ce47439bacfa801d4af93f
Adam <adam1092002@icloud.com>
2024-02-04 21:57:13 +0000

    Created roles in identityuser

---
+++ CallRegister.Models/ApplicationUSer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallRegister.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        [Required]
+        public int Name { get; set; }
+
+        public string? JobTitle { get; set; }
+    }
+    
+    
+}
