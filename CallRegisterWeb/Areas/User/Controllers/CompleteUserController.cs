00ab4f329203ceaf87efd60fcc44f0276f70a365
Adam <adam1092002@icloud.com>
2024-03-04 22:06:36 +0000

    Styled Phonecall Menus

--- CallRegisterWeb/Areas/User/Controllers/CompleteUserController.cs
+++ CallRegisterWeb/Areas/User/Controllers/CompleteUserController.cs
@@ -25,37 +25,18 @@ namespace CallRegisterWeb.Areas.User.Controllers
             return View(objAgentsList);
 
         }
-        //    public IActionResult Edit(string? id)
-        //    {
-        //        r
+ 
         public IActionResult Edit(string? id)
         {
-            //if (id == null || id == 0)
-            //{
-            //    return NotFound();
-            //}
-
             List<PhoneCall> callsFromDb = _unitOfWork.PhoneCallRepository.GetIncomplete(u => u.Agent == id && u.Complete == false, includeProperties: "Products").ToList();
             if (callsFromDb == null)
             {
                 return NotFound();
             }
             return View(callsFromDb);
+            //Viewdata for reirect?
         }
 
-        //public IActionResult UpdateComplete(string? objId)
-        //{
-        //    //if (ModelState.IsValid)
-        //    //{
-       
-        //    //phoneCall.Complete = true;
-        //    //_unitOfWork.PhoneCallRepository.Update(phoneCall);
-        //    //_unitOfWork.Save();
-        //    //    //TempData["success"] = "Product Updated Successfully";
-        //    //    return RedirectToAction("Index", "Products");
-        //    //}
-        //    return View();
-        //}
         public IActionResult UpdateComplete(int? objid)
         {
             PhoneCall phoneCall = _unitOfWork.PhoneCallRepository.Get(u => u.Id == objid);
@@ -63,7 +44,7 @@ namespace CallRegisterWeb.Areas.User.Controllers
             _unitOfWork.PhoneCallRepository.Update(phoneCall);
             _unitOfWork.Save();
 
-            return View();
+            return RedirectToAction(actionName: "Index");
         }
     }
 }
