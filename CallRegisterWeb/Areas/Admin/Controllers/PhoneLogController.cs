c498f3d1f7fc6627fc1606a8e9777a75e877cf9a
Adam <adam1092002@icloud.com>
2024-03-07 22:17:17 +0000

    Datatables for PhoneCall Log

--- CallRegisterWeb/Areas/Admin/Controllers/PhoneLogController.cs
+++ CallRegisterWeb/Areas/Admin/Controllers/PhoneLogController.cs
@@ -22,8 +22,9 @@ namespace CallRegisterWeb.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<PhoneCall> objProductsList = _unitOfWork.PhoneCallRepository.GetAll(includeProperties: "Products").ToList();
-
             return View(objProductsList);
+
+
         }
 
         //public IActionResult Create()
@@ -44,60 +45,19 @@ namespace CallRegisterWeb.Areas.Admin.Controllers
         //    return View();
         //}
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
 
-            Products? productFromDb = _unitOfWork.ProductsRepository.Get(u => u.Id == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Products obj)
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.ProductsRepository.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product Updated Successfully";
-                return RedirectToAction("Index", "Products");
-            }
-            return View();
+            List<PhoneCall> objPhoneList = _unitOfWork.PhoneCallRepository.GetAll(includeProperties: "Products").ToList();
+            return Json(new { data = objPhoneList });
         }
 
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
 
-            Products? agentFromDb = _unitOfWork.ProductsRepository.Get(u => u.Id == id);
-            if (agentFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(agentFromDb);
-        }
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePost(int? id)
-        {
-            Products? obj = _unitOfWork.ProductsRepository.Get(u => u.Id == id);
-            if (obj == null)
-            {
-                return NotFound(id);
-            }
-            _unitOfWork.ProductsRepository.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Agent Deleted Successfully";
-            return RedirectToAction("Index");
-        }
 
+
+        #endregion
     }
 }
---
+++ CallRegisterWeb/wwwroot/js/phonelog.js
@@ -0,0 +1,24 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/phonelog/getall' },
+        "columns": [
+            { data: 'id', "width": "15%" },
+            { data: 'agent', "width": "15%" },
+            { data: 'allocatedDate', "width": "15%" },
+            { data: 'dateDue', "width": "15%" },
+            { data: 'dateCompleted', "width": "15%" },
+            { data: 'internal', "width": "15%" },
+            { data: 'products.name', "width": "15%" }
+
+        ]
+    });
+}
+
+
+
